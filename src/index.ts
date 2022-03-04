0ee8baa6b1cf09404ac84f065a54a83f0c6fad79
erba-28 <erba-28@qq.com>
2022-03-05 06:59:43 +0800

    feat: app listening

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log(`Hi, tun-create-app`)
+import { TunApplication } from '@tunframework/tun'
+import { bodyparser } from '@tunframework/tun-bodyparser'
+import { RestifyRouter } from '@tunframework/tun-rest-router'
+import { AddressInfo } from 'node:net'
+
+const app = new TunApplication()
+app.use(bodyparser())
+
+const router = new RestifyRouter()
+  .get('/', async (ctx, next) => `Hi, world!`)
+  .post('/', async (ctx, next) => JSON.stringify(ctx.req.body))
+  .put('/{id}', async (ctx, next) => `${ctx.req.slugs.id}`)
+  .delete('/:id', async (ctx, next) => `${ctx.req.slugs.id}`)
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+const server = app.listen({ host: '127.0.0.1', port: 3000 })
+
+server.on('listening', async () => {
+  let addr = (server.address() || {}) as AddressInfo
+  console.log(`listening: http://${addr.address}:${addr.port}`)
+})
